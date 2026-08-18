a3409c83aae6d22d1504d9dcfbe34ed134d6c2da
livia <liviamachado.txu@gmail.com>
2026-08-17 21:57:03 -0300

    sorteio

--- app/src/main/java/com/ifsc/app/MainActivity.java
+++ app/src/main/java/com/ifsc/app/MainActivity.java
@@ -2,15 +2,17 @@ package com.ifsc.app;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView TextView;
-
-    int contador=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,34 +23,35 @@ public class MainActivity extends AppCompatActivity {
         Button b=findViewById(R.id.button);
         TextView tv = findViewById(R.id.tv);
 
-        tv.setText(Integer.toString(contador));
+        EditText edmin,edmax;
+        edmin = findViewById(R.id.edMin);
+        edmax = findViewById(R.id.edMax);
+        tv.setText("Número Aleatório");
+
         b.setOnClickListener(view -> {
-            contador++;
-            tv.setText(Integer.toString(contador));
+            String smin = edmin.getText().toString();
+            String smax = edmax.getText().toString();
+
+            if(smin.isEmpty()){
+                edmin.setError("Informe um número");
+                return;
+            }
+            if(smax.isEmpty()){
+                edmax.setError("Informe um número");
+                return;
+            }
+
+            int min = Integer.parseInt(edmin.getText().toString());
+            int max = Integer.parseInt(edmax.getText().toString());
+
+            if(min>max){
+                Toast.makeText(this, "Informe um número Máximo maior que o Minimo", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Random random = new Random();
+            int randomN = random.nextInt(min, max);
+            tv.setText(Integer.toString(randomN));
         });
-
-        /*View textView = findViewById((R.id.main));
-        textView.setText(contador);
-        textView.setOnClickListener(V -> {
-            textView.setText("BLA");
-        });
-
-
-        /*setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        Button button = findViewById(R.id.button);
-        TextView TextView = findViewById(R.id.textView);
-
-        button.setOnClickListener(view -> {
-            contador++;
-
-            textView.setText(Integer.toString(contador));
-
-        });*/
     }
 }
